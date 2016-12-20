04f3ccc4a0714cb74d8b57681f965f39145cf6ac
Victor Ferre <victor.ferre@gmail.com>
2016-12-20 17:49:21 +0100

    Actualización a ASP.NET Core 1.1 y algunos cambios.

--- src/LearnPolymer/Startup.cs
+++ src/LearnPolymer/Startup.cs
@@ -19,6 +19,12 @@ namespace LearnPolymer
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
             Configuration = builder.Build();
         }
 
@@ -28,6 +34,8 @@ namespace LearnPolymer
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
+            services.AddApplicationInsightsTelemetry(Configuration);
+
             services.AddMvc();
         }
 
@@ -36,6 +44,8 @@ namespace LearnPolymer
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            app.UseApplicationInsightsRequestTelemetry();
 
             if (env.IsDevelopment())
             {
@@ -46,6 +56,8 @@ namespace LearnPolymer
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            app.UseApplicationInsightsExceptionTelemetry();
 
             app.UseStaticFiles();
 
--- src/LearnPolymer/wwwroot/_references.js
+++ src/LearnPolymer/wwwroot/_references.js
@@ -1,2 +1,3 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="js/site.js" />
+/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
